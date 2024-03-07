bc245dc7df1d9281c31e658ef2463b2bcc6295d4
DevRahat <dev.rahatislam@gmail.com>
2024-03-07 17:51:36 +0600

    first commit

---
+++ es6.js
@@ -0,0 +1,6 @@
+const numbers=[12,23,34,45,67];
+const newNumbers=[...numbers,55];
+console.log(numbers);
+console.log(newNumbers);
+newNumbers.push(78);
+console.log(newNumbers);
---
+++ fundamentals.js
@@ -0,0 +1,9 @@
+const student={
+    name: 'Tamim',
+    age: 32,
+    movies:['King khan','Dhakar mastan']
+}
+const myVariable='age'
+console.log(student[myVariable]);
+console.log(student.age);
+console.log(student['age']);
