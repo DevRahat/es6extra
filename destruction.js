d0c0244f971b9492aaa9aa1e50b985126209d96c
DevRahat <dev.rahatislam@gmail.com>
2024-03-07 19:29:49 +0600

    4th commit

---
+++ destruction.js
@@ -0,0 +1,13 @@
+const person={
+    name: 'Ananta',
+    age: 40,
+    job: 'Actor',
+    movies: ['khoj the search','Jani na']
+}
+
+const {name:identity}=person;
+console.log(identity);
+
+const list=['laptop','phone','cable'];
+const [divice1,divice2,divice3]=list;
+console.log(divice1);
